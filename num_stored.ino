6a9209c10bf5ca1ebd1a99ea387345a7071e185a
rajugokul <rajugokul1996@gmail.com>
2017-12-19 15:05:02 +0530

    Add files via upload

---
+++ num_stored.ino
@@ -0,0 +1,65 @@
+/*
+ * EEPROM Read
+ *
+ * Reads the value of each byte of the EEPROM and prints it
+ * to the computer.
+ * This example code is in the public domain.
+ */
+
+#include <EEPROM.h>
+
+// start reading from the first byte (address 0) of the EEPROM
+int addr = 0;
+int val=0;
+String value="";
+
+void setup() {
+  // initialize serial and wait for port to open:
+  Serial.begin(9600);
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
+
+
+}
+
+void loop() {
+  
+  // read a byte from the current address of the EEPROM
+  
+  /***
+    Advance to the next address, when at the end restart at the beginning.
+
+    Larger AVR processors have larger EEPROM sizes, E.g:
+    - Arduno Duemilanove: 512b EEPROM storage.
+    - Arduino Uno:        1kb EEPROM storage.
+    - Arduino Mega:       4kb EEPROM storage.
+
+    Rather than hard-coding the length, you should use the pre-provided length function.
+    This will make your code portable to all AVR processors.
+  ***/
+//  address = address + 1;
+//  if (address == EEPROM.length()) {
+//    address = 0;
+//  }
+//
+//  /***
+//    As the EEPROM sizes are powers of two, wrapping (preventing overflow) of an
+//    EEPROM address is also doable by a bitwise and of the length - 1.
+//
+//    ++address &= EEPROM.length() - 1;
+//  ***/
+//
+//  delay(500);
+}
+int s(){
+  value = EEPROM.write(addr,val);
+
+  Serial.print(addr);
+  Serial.print("\t");
+  Serial.print(val);
+  Serial.println();
+  return 0;
+
+  
+}
